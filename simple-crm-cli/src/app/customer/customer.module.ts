a643e3a5fb43d281c90ba7e086a1eb1b26c1daf6
Michael Lang <michael.lang@nexul.com>
2019-08-24 20:10:06 -0500

    Step006: customer service created

--- simple-crm-cli/src/app/customer/customer.module.ts
+++ simple-crm-cli/src/app/customer/customer.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomerListPageComponent } from './customer-list-page/customer-list-page.component';
 import { SharedImportsModule } from '../shared/shared-imports.module';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [CustomerListPageComponent],
   imports: [
     CommonModule,
+    HttpClientModule,
     CustomerRoutingModule,
     SharedImportsModule
   ]
