89025b7b2fdd2294b357e43651be33af2dbc843b
Michael Lang <michael.lang@nexul.com>
2019-09-02 12:00:04 -0500

    Step101: new C# database layer class library project

---
+++ Classroom.SimpleCRM.SqlDbServices/CrmDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Classroom.SimpleCRM.SqlDbServices
+{
+    public class CrmDbContext : DbContext
+    {
+        public CrmDbContext(DbContextOptions<CrmDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Customer> Customer { get; set; }
+    }
+}
---
+++ Classroom.SimpleCRM.SqlDbServices/SqlCustomerData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classroom.SimpleCRM.SqlDbServices
+{
+    public class SqlCustomerData : ICustomerData
+    {
+        public Customer Get(int customerId)
+        {
+            throw new NotImplementedException();
+        }
+        public void Add(Customer item)
+        {
+            throw new NotImplementedException();
+        }
+        public void Update(Customer item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Commit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
