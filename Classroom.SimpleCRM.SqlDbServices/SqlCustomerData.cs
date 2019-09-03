363aca7b0809197515eafa3cf07e1faa84b47328
Michael Lang <michael.lang@nexul.com>
2019-09-02 21:44:44 -0500

    Step105: implement SqlCustomerData repo with custom sort

--- Classroom.SimpleCRM.SqlDbServices/SqlCustomerData.cs
+++ Classroom.SimpleCRM.SqlDbServices/SqlCustomerData.cs
@@ -1,27 +1,61 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using System.Linq.Dynamic.Core;
 
 namespace Classroom.SimpleCRM.SqlDbServices
 {
     public class SqlCustomerData : ICustomerData
     {
+        private readonly CrmDbContext context;
+
+        public SqlCustomerData(CrmDbContext context)
+        {
+            this.context = context;
+        }
+
         public Customer Get(int customerId)
         {
-            throw new NotImplementedException();
+            return context.Customer.FirstOrDefault(x => x.CustomerId == customerId);
+        }
+        public List<Customer> GetByStatus(int accountId, CustomerStatus status, int pageIndex, int take, string orderBy)
+        {
+            var sortableFields = new string[] { "FIRSTNAME", "LASTNAME", "EMAILADDRESS", "PHONENUMBER", "STATUS", "LASTCONTACTDATE" };
+            var fields = (orderBy ?? "").Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var field in fields)
+            {
+                var x = field.Trim().ToUpper();
+                var parts = x.Split(' ');
+                if (parts.Length > 2)
+                    throw new ArgumentException("Invalid sort option " + x);
+                if (parts.Length > 1 && parts[1].ToUpper() != "DESC" && parts[1].ToUpper() != "ASC")
+                    throw new ArgumentException("Invalid sort direction " + x);
+                if (!sortableFields.Contains(x))
+                    throw new ArgumentException("Invalid sort field " + x);
+            } //all sort requested fields are valid.
+            return context.Customer
+                .Where(x => x.Status == status)
+                .OrderBy(orderBy) //validated above to nothing unexpected, this is OK now
+                .Skip(pageIndex * take)
+                .Take(take)
+                .ToList();
         }
         public void Add(Customer item)
         {
-            throw new NotImplementedException();
+            context.Add(item); //not inserted to database yet, just pending insert
         }
         public void Update(Customer item)
         {
-            throw new NotImplementedException();
+            //do nothing, changes are tracked automatically
         }
-
+        public void Delete(Customer item)
+        {
+            context.Remove(item);
+        }
         public void Commit()
         {
-            throw new NotImplementedException();
+            context.SaveChanges();
         }
     }
 }
