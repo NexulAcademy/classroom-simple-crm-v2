ac088276b4c7ac6675f493a14e3d639c9fb5a1df
Michael Lang <michael.lang@nexul.com>
2019-09-04 07:59:11 -0500

    Step111: API view model validation rules

--- Classroom.SimpleCRM.WebApi/Models/CustomerCreateViewModel.cs
+++ Classroom.SimpleCRM.WebApi/Models/CustomerCreateViewModel.cs
@@ -1,10 +1,17 @@
-﻿namespace Classroom.SimpleCRM.WebApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Classroom.SimpleCRM.WebApi.Models
 {
     public class CustomerCreateViewModel
     {
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Phone]
         public string PhoneNumber { get; set; }
+        [Required]
+        [EmailAddress]
         public string EmailAddress { get; set; }
         public InteractionMethod PreferredContactMethod { get; set; }
     }
--- Classroom.SimpleCRM.WebApi/Models/CustomerUpdateViewModel.cs
+++ Classroom.SimpleCRM.WebApi/Models/CustomerUpdateViewModel.cs
@@ -1,13 +1,20 @@
-﻿namespace Classroom.SimpleCRM.WebApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Classroom.SimpleCRM.WebApi.Models
 {
     /// <summary>
-    /// Update is currently the same as update model, but in the future they will likely diverge.
+    /// Update is currently the same as create model, but in the future they will likely diverge.
     /// </summary>
     public class CustomerUpdateViewModel
     {
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Phone]
         public string PhoneNumber { get; set; }
+        [Required]
+        [EmailAddress]
         public string EmailAddress { get; set; }
         public InteractionMethod PreferredContactMethod { get; set; }
     }
