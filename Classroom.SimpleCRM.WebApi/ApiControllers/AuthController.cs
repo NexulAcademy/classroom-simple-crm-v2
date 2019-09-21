38f6698a8983de49129872d882a386705a8c1009
Michael Lang <michael.lang@nexul.com>
2019-09-21 09:11:56 -0500

    Step 126: create AuthController and CredentialsViewModel

---
+++ Classroom.SimpleCRM.WebApi/ApiControllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Classroom.SimpleCRM.WebApi.Filters;
+using Classroom.SimpleCRM.WebApi.Models.Auth;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Classroom.SimpleCRM.WebApi.ApiControllers
+{
+    public class AuthController : Controller
+    {
+        [HttpPost("login")]
+        public async Task<IActionResult> Post([FromBody]CredentialsViewModel credentials)
+        {
+            if (!ModelState.IsValid)
+            {
+                return new ValidationFailedResult(ModelState);
+            }
+
+            var user = await Authenticate(credentials.EmailAddress, credentials.Password);
+            if (user == null)
+            {
+                return new ValidationFailedResult("Invalid username or password.");
+            }
+
+            var userModel = await GetUserData(user);
+            return Ok(userModel);
+        }
+    }
+}
