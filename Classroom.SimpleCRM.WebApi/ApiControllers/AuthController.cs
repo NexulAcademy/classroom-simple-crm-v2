f2fdf6dd9cf32349f9d049f9f9d00c69211cbf1c
Michael Lang <michael.lang@nexul.com>
2019-09-21 09:15:10 -0500

    Step 127: Authenticate name/password with ASP.Net Identity

--- Classroom.SimpleCRM.WebApi/ApiControllers/AuthController.cs
+++ Classroom.SimpleCRM.WebApi/ApiControllers/AuthController.cs
@@ -1,12 +1,20 @@
 ﻿using System.Threading.Tasks;
 using Classroom.SimpleCRM.WebApi.Filters;
 using Classroom.SimpleCRM.WebApi.Models.Auth;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Classroom.SimpleCRM.WebApi.ApiControllers
 {
     public class AuthController : Controller
     {
+        private readonly UserManager<CrmIdentityUser> _userManager;
+
+        public AuthController(UserManager<CrmIdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         [HttpPost("login")]
         public async Task<IActionResult> Post([FromBody]CredentialsViewModel credentials)
         {
@@ -23,6 +31,26 @@ namespace Classroom.SimpleCRM.WebApi.ApiControllers
 
             var userModel = await GetUserData(user);
             return Ok(userModel);
+        }
+
+        private async Task<CrmIdentityUser> Authenticate(string emailAddress, string password)
+        {
+            if (string.IsNullOrEmpty(emailAddress) || string.IsNullOrEmpty(password))
+                return await Task.FromResult<CrmIdentityUser>(null);
+
+            // get the user to verifty
+            var userToVerify = await _userManager.FindByNameAsync(emailAddress);
+
+            if (userToVerify == null) return await Task.FromResult<CrmIdentityUser>(null);
+
+            // check the credentials
+            if (await _userManager.CheckPasswordAsync(userToVerify, password))
+            {
+                return await Task.FromResult(userToVerify);
+            }
+
+            // Credentials are invalid, or account doesn't exist
+            return await Task.FromResult<CrmIdentityUser>(null);
         }
     }
 }
