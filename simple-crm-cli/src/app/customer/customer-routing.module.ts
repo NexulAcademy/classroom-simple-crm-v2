e973e9da762e98660cdbf735d5a0e22c81759e2c
Michael Lang <michael.lang@nexul.com>
2019-10-06 22:31:08 -0500

    Step200: Add RouteGuard - AuthenticatedGuard

--- simple-crm-cli/src/app/customer/customer-routing.module.ts
+++ simple-crm-cli/src/app/customer/customer-routing.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CustomerListPageComponent } from './customer-list-page/customer-list-page.component';
 import { CustomerDetailComponent } from './customer-detail/customer-detail.component';
+import { AuthenticatedGuard } from '../account/authenticated.guard';
 
 
 const routes: Routes = [
   {
     path: 'customers',
     pathMatch: 'full',
-    component: CustomerListPageComponent
+    component: CustomerListPageComponent,
+    canActivate: [AuthenticatedGuard]
   },
   {
     path: 'customer/:id',
     pathMatch: 'full',
-    component: CustomerDetailComponent
+    component: CustomerDetailComponent,
+    canActivate: [AuthenticatedGuard]
   }
 ];
 
