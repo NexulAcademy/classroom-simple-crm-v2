93e8a3d87e872a818b129590b89654449d4f461a
Michael Lang <michael.lang@nexul.com>
2019-10-27 23:11:48 -0500

    Step204: search filter combineLatest, debounceTime

--- simple-crm-cli/src/app/customer/customer-list-page/customer-list-page.component.ts
+++ simple-crm-cli/src/app/customer/customer-list-page/customer-list-page.component.ts
@@ -5,6 +5,9 @@ import { CustomerService } from '../customer.service';
 import { Observable } from 'rxjs';
 import { CustomerCreateDialogComponent } from '../customer-create-dialog/customer-create-dialog.component';
 import { Router } from '@angular/router';
+import { FormControl } from '@angular/forms';
+import { combineLatest } from 'rxjs';
+import { startWith, map, debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'crm-customer-list-page',
@@ -14,6 +17,7 @@ import { Router } from '@angular/router';
 export class CustomerListPageComponent implements OnInit {
   customers$: Observable<Customer[]>;
   displayColumns = ['icon', 'name', 'phone', 'email', 'lastContactDate', 'status', 'actions'];
+  filterInput = new FormControl();
 
   constructor(
     private customerService: CustomerService,
@@ -22,7 +26,7 @@ export class CustomerListPageComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.customers$ = this.customerService.search('');
+    this.search();
   }
 
   openDetail(item: Customer) {
@@ -31,13 +35,29 @@ export class CustomerListPageComponent implements OnInit {
     }
   }
 
+  search() {
+    const fString$: Observable<string> = this.filterInput.valueChanges.pipe(
+      startWith(''),
+      debounceTime(700)
+    );
+    this.customers$ = combineLatest([this.customerService.search(''), fString$]).pipe(
+      map(([customers, fString]) => {
+        return customers.filter(cust => {
+          return (cust.firstName + ' ' + cust.lastName).indexOf(fString) >= 0;
+        });
+      })
+    );
+  }
+
   addCustomer() {
     const dialogRef = this.dialog.open(CustomerCreateDialogComponent, {
       width: '250px',
       data: null
     });
     dialogRef.afterClosed().subscribe((customer: Customer) => {
-      this.customerService.save(customer);
+      this.customerService.save(customer).subscribe(result => {
+        this.search();
+      });
     });
   }
 
